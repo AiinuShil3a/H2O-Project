02461d465f9f21d4c7d776b75d044382c93feb9b
Aiinushil3a <Suppha1ak.sm@outlook.com>
2024-03-08 05:59:10 +0700

    v0.0.3

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,25 +1,94 @@
-import React from 'react';
+import { useState } from "react";
 
 const Search = () => {
+  const [isPackage, setIsPackage] = useState(false);
+  const clickToPackage = () => {
+    setIsPackage(true)
+  }
+  const clickToHome = () => {
+    setIsPackage(false)
+  }
   return (
-    <div className="relative bg-red p-8 h-[500px] flex items-center">
+    <div
+      className="relative p-8 h-[500px] flex items-center"
+      style={{
+        backgroundImage:
+          'url("https://wallpapertag.com/wallpaper/full/8/3/c/829601-large-tropical-beach-desktop-backgrounds-2560x1600-for-retina.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div className="mx-auto">
-        <div className="relative bg-blue p-6 rounded  w-[50vw] h-[35vh] flex flex-col items-center justify-center">
-          {/* Search input */}
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-white text-black rounded p-2 mb-4 block w-[30vw]"
-          />
-          {/* Buttons container */}
-          <div className="flex items-center justify-between mt-4">
-            {/* Number of people selection */}
-            <button className="bg-white text-black rounded p-2 mr-2">Select Number of People</button>
-
-            {/* Calendar selection */}
-            <button className="bg-white text-black rounded p-2">Select Calendar</button>
+        {isPackage === false && (
+          <div className="relative bg-white bg-opacity-50 p-6 rounded-[20px] w-[50vw] h-[35vh] flex flex-col items-center justify-center shadow-lg">
+            <div className="flex items-center justify-between mt-4">
+              <div className="relative">
+                <button className="bg-blue text-white p-2 mr-2 rounded-[15px]" onClick={clickToHome}>
+                  สถานที่พัก
+                </button>
+              </div>
+              <div className="relative">
+                <button className="bg-white text-black p-2 rounded-[15px]" onClick={clickToPackage}>
+                  แพ็คเกจ
+                </button>
+              </div>
+            </div>
+            <hr className="w-full my-5 border-t-2 border-white shadow-lg" />
+            <div className="flex items-center justify-center">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-white text-black p-2 mb-2 rounded-tl-[10px] rounded-bl-[10px] block w-[25vw] relative" // หรือเปลี่ยนเป็น w-[100%]
+              />
+              <button className="bg-blue text-black p-2 mb-2 block text-white rounded-tr-[10px] rounded-br-[10px]">
+                ค้นหา
+              </button>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <button className="bg-white text-black rounded-[10px] p-2 mr-2">
+                Select People
+              </button>
+              <button className="bg-white text-black rounded-[10px] p-2">
+                Select Calendar
+              </button>
+            </div>
           </div>
-        </div>
+        )}
+        {isPackage === true && (
+          <div className="relative bg-white bg-opacity-50 p-6 rounded-[20px] w-[50vw] h-[35vh] flex flex-col items-center justify-center shadow-lg">
+            <div className="flex items-center justify-between mt-4">
+              <div className="relative">
+                <button className="bg-white text-black p-2 mr-2 rounded-[15px]" onClick={clickToHome}>
+                  สถานที่พัก
+                </button>
+              </div>
+              <div className="relative">
+                <button className="bg-blue text-white p-2 rounded-[15px]" onClick={clickToPackage}>
+                  แพ็คเกจ
+                </button>
+              </div>
+            </div>
+            <hr className="w-full my-5 border-t-2 border-white shadow-lg" />
+            <div className="flex items-center justify-center">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-white text-black p-2 mb-2 rounded-tl-[10px] rounded-bl-[10px] block w-[25vw] relative" // หรือเปลี่ยนเป็น w-[100%]
+              />
+              <button className="bg-blue text-black p-2 mb-2 block text-white rounded-tr-[10px] rounded-br-[10px]">
+                ค้นหา
+              </button>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <button className="bg-white text-black rounded-[10px] p-2 mr-2">
+                Select Type Package
+              </button>
+              <button className="bg-white text-black rounded-[10px] p-2">
+                Select Calendar
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
