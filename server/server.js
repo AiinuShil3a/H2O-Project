40b5935c4f57c1443420022d6568e02b00dc07c7
PXTN2 <150564535+PXTN2@users.noreply.github.com>
2024-03-07 22:35:43 +0700

    config .env and connect mongo

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 const express = require("express")
 const cors = require("cors")
+const router = express.Router();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
+dotenv.config();
 
 
 const app = express();
-// app.use(cors({ credentials: true, origin: CLIENT_URL }));
-// app.use(express.json());
+const CLIENT_URL = process.env.CLIENT_URL;
+app.use(cors({ credentials: true, origin: CLIENT_URL }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const MONGODB_URL = process.env.MONGODB_URL;
+mongoose.connect(MONGODB_URL);
 
 app.get("/", (req, res) => {
-    res.send("<h1> Welcome to restful API Blog</h1>");
+    res.send("<h1> Welcome to H2O Project</h1>");
   });
 
-const PORT = 6000;
-const server = app.listen(PORT, () => {
+  const PORT = process.env.PORT;
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
 });
